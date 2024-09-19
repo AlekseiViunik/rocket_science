e72f56c31f84d3b3e3de55e431a9d74b85024e96
Aleksei Viunik <a79643739989@yandex.com>
2024-09-19 13:25:30 +0200

    Add logic to controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Product::query();
+
+        // Если есть фильтры по свойствам
+        if ($request->has('properties')) {
+            $properties = $request->input('properties');
+
+            foreach ($properties as $propertyName => $values) {
+                $query->whereHas('propertyValues.property', function ($q) use ($propertyName, $values) {
+                    $q->where('name', $propertyName)
+                        ->whereIn('value', $values);
+                });
+            }
+        }
+
+        // Получаем товары с их свойствами и пагинацией по 40 элементов
+        $products = $query->with('propertyValues.property')->paginate(40);
+
+        return response()->json($products);
+    }
 }
